839667db461db999821fff18052e56252245b735
Irishtheboy <109201974+Irishtheboy@users.noreply.github.com>
2025-12-15 12:48:49 +0200

    Fixed mobile About alignment and added polish features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import Header from './components/Header/Header';
 import ScrollProgress from './components/ScrollProgress/ScrollProgress';
+import BackToTop from './components/BackToTop/BackToTop';
 import Entry from './components/Entry/Entry';
 import Home from './pages/Home/Home';
 import Portfolio from './pages/Portfolio/Portfolio';
@@ -13,25 +15,28 @@ import './App.css';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="App">
-          <ScrollProgress />
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Entry />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="*" element={<Entry />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <Router>
+          <div className="App">
+            <ScrollProgress />
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<Entry />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="*" element={<Entry />} />
+              </Routes>
+            </main>
+            <Footer />
+            <BackToTop />
+          </div>
+        </Router>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
