7134c54c5e3828a15ccf7e6fd546f34a469695ba
Irishtheboy <109201974+Irishtheboy@users.noreply.github.com>
2025-12-15 13:29:49 +0200

    Move YOU loader above welcome text

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -5,7 +5,6 @@ const LoadingScreen = () => {
   return (
     <div className="loading-screen">
       <div className="loading-content">
-        <h1 className="welcome-text">Welcome to KILLYDID</h1>
         <div className="loader">
           <svg height="0" width="0" viewBox="0 0 64 64" className="absolute">
             <defs xmlns="http://www.w3.org/2000/svg">
@@ -35,6 +34,7 @@ const LoadingScreen = () => {
             <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="8" stroke="url(#d)" d="M 4,4 h 4.6230469 v 25.919922 c -0.00276,11.916203 9.8364941,21.550422 21.7500001,21.296875 11.616666,-0.240651 21.014356,-9.63894 21.253906,-21.25586 a 2.0002,2.0002 0 0 0 0,-0.04102 V 4 H 56.25 v 25.919922 c 0,14.33873 -11.581192,25.919922 -25.919922,25.919922 a 2.0002,2.0002 0 0 0 -0.0293,0 C 15.812309,56.052941 3.998433,44.409961 4,29.919922 Z" className="dash" pathLength="360"></path>
           </svg>
         </div>
+        <h1 className="welcome-text">Welcome to KILLYDID</h1>
       </div>
     </div>
   );
